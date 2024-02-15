3a90145fe6bbc4c70a0c359c3446bd0d81d90ac1
root <root@TI05.mac.local>
2024-02-15 17:55:08 -0300

    feat: add responsive size on font

--- src/components/EmptList/styles.ts
+++ src/components/EmptList/styles.ts
@@ -5,13 +5,22 @@ export const Main = styled('main', {
     flexDirection: 'column',
     alignItems: 'center',
     width: '100%',
-    height: '50px',
+    padding: '2rem 1rem',
     borderTop: '1px solid $gray-500',
     borderRadius: '8px',
     color: '$gray-300',
-    padding: '4rem',
+
+
+    
     
     strong: {
+        textAlign: 'center',
         marginTop: '1.5rem',
+        fontSize: "clamp(.8rem, 1.5vw, 2rem)"
+    },
+
+    p: {
+        fontSize: "clamp(.6rem, 1.2vw, 1.5rem)",
+        textAlign: 'center',
     }
 })
